b362b0a67a2ec3c94c2863b18b80f986a1eafb35
wenlihaoyu <wenyinlong52@163.com>
2016-08-23 17:49:33 +0800

    add mongoconfig.py file

---
+++ config/mongoconfig.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 23 15:11:57 2016
+
+@author: lywen
+"""
+
+def getmongo():
+    """获取mongo配置"""
+    user = 'option'
+    password ='option'
+    host='127.0.0.1'
+    port=27017
+    db='option'
+    return user,password,host,port,db
+    
